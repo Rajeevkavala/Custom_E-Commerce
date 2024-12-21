5689e9d1c15e237c6a39f7221b4a6746709beb25
Rajeevkavala <68290177+Rajeevkavala@users.noreply.github.com>
2024-12-21 19:49:35 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,2 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
+export const BASE_URL = "https://custom-e-commerce-backend.onrender.com/";
+export const USERS_URL = "https://custom-e-commerce-backend.onrender.com/api/users";
